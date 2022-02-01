f2c503e7374f3e5f7ee2b84d6c70cfd0a3bfff3f
pauline-wim <linette.wim@hotmail.com>
2022-02-01 15:39:45 +0100

    01 - App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      france: [],
+      name: "",
+      capital: "",
+      flag: "",
+      population: "",
+      region: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.com/v2/name/france")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({ france: res });
+        this.state.france.map((elem) => {
+          return this.setState({
+            name: elem.name,
+            capital: elem.capital,
+            flag: elem.flag,
+            population: elem.population,
+            region: elem.region,
+          });
+        });
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // attention aux setState ici, risque de boucle infinie
+    console.log(prevState, this.state);
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Country selector</h1>
+        <div>
+          <p>Name: {this.state.name}</p>
+          <p>Capital: {this.state.capital}</p>
+          <p>Flag: {this.state.flag}</p>
+          <p>Population: {this.state.population}</p>
+          <p>Region: {this.state.region}</p>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
